c1406f61f45d5d8c3490601cbed8fc0552418f99
DaveParisi <david.parisi90@gmail.com>
2025-07-05 18:29:13 -0400

    Updated landing page

--- bcwebstore/bcwebstore/src/App.js
+++ bcwebstore/bcwebstore/src/App.js
@@ -1,15 +1,35 @@
 import logo from './logo.jpg';
 import './App.css';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <title>Bloom Circuit - Modded Custom Handheld Consoles</title>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p id="Mainparagraph">
-          Coming Soon 
-        </p>
+      <title>Bloom Circuit - Modded Handheld Consoles</title>
+        <Link to="/">
+          <img src={logo} className="App-logo" alt="Bloom Circuit logo" />
+        </Link>
+
+        <div id="Desc-wrapper">
+          <p id="Main-landing-text">
+            Bloom Circuit's webpage is coming soon
+          </p>
+
+          <p id="eBay-link">
+            For active listings visit our{' '}
+            <a
+              href="https://www.ebay.com/usr/bloomcircuit"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              eBay store
+            </a>{' '}
+            or <a href="mailto:info@bloom-circuit.com">email us</a>.
+          </p>
+
+          <p id="built-by">This webpage is built with React</p>
+        </div>
       </header>
     </div>
   );
