987ed1e39430c18fa167158c2beaaecf25a32d8b
DaveParisi <david.parisi90@gmail.com>
2025-07-05 17:05:31 -0400

    Updated to REACT app for web format

---
+++ bcwebstore/bcwebstore/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <title>Bloom Circuit - Modded Custom Handheld Consoles</title>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p id="Mainparagraph">
+          Coming Soon 
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
